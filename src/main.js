edf1272b268d4b0599d7e37adaa0fa66cc5acbf9
taopaodaheng <3206986878@qq.com>
2021-08-10 16:59:26 +0800

    项目引入vant组件库

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import store from './store'
 // 加载全局样式
 import './styles/index.css'
+// 加载Vant核心组件库
+import Vant from 'vant'
+// 加载Vant全局样式
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
